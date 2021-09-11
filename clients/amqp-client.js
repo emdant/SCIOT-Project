f204ae39d1e5a7d2def5274fc19e802157441812
Marco <30555419+emdant@users.noreply.github.com>
2021-09-11 19:31:13 +0200

    Implemented client and consumer draft

--- clients/amqp-client.js
+++ clients/amqp-client.js
@@ -1,13 +1,37 @@
 const amqp = require('amqplib');
+const { codes } = require('./fiscalcodes.json');
+const { buildings } = require('./places.json');
 
-const queue = '/iot/scanners';
-amqp.connect('amqp://localhost:5672').then(async (connection) => {
-  const channel = await connection.createChannel();
-  await channel.assertQueue(queue, { durable: false });
-  channel.sendToQueue(queue, Buffer.from('Hello World!'));
-  console.log('Sent message to queue');
+const exchange = 'iot/scanners'
+const key = 'person';
 
-  await channel.close();
-  await connection.close()
-})
-.catch(console.warn);
+setInterval(() => {
+  amqp.connect('amqp://localhost:5672').then(async (connection) => {
+    const channel = await connection.createChannel();
+  
+    await channel.assertExchange(exchange, 'topic', {durable: false})
+  
+    channel.publish(exchange, key, Buffer.from(JSON.stringify(randomData())));
+  
+    console.log('Sent message to exchange');
+  
+    await channel.close();
+    await connection.close()
+  })
+  .catch(console.warn);
+}, 5000);
+
+function randomData() {
+  let building = buildings[randomInt(buildings.length)]
+
+  return {
+    "fiscalcode": codes[randomInt(codes.length)],
+    "building": building.name,
+    "room": building.rooms[randomInt(building.rooms.length)],
+    "datetime": new Date().toISOString()
+  }
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * (max));
+}
--- clients/amqp-receiver.js
+++ clients/amqp-receiver.js
@@ -3,10 +3,19 @@ var amqp = require('amqplib');
 const queue = '/iot/scanners';
 amqp.connect('amqp://localhost:5672').then(async (connection) => {
   const channel = await connection.createChannel();
-  await channel.assertQueue(queue, {durable: false});
+
+  await channel.assertQueue(queue, {
+      durable: true
+    });
+  await channel.prefetch(1);
+
   await channel.consume(queue, (msg) => {
-    console.log(msg.content.toString());
-  }, {noAck: true});
+    console.log(JSON.parse(msg.content.toString()));
+
+    channel.ack(msg); // acknowledge
+  }, {
+    noAck: false // manual acknoledgment
+  });
   
   await channel.close();
   await connection.close();
